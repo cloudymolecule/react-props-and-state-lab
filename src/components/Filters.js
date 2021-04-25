9a69034c4b5487b0718a0862be3e99c3b4871f96
Cloudymolecule <baires82@gmail.com>
2021-04-25 14:56:16 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
 class Filters extends React.Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      pet: 'all'
+    }
+  }
+  
+  handleChange = event => {
+    this.setState({
+      pet: event.target.value
+    }, () => this.props.onChangeType(this.state.pet))
+  }
+
+  getThemAll = () => {
+    this.props.onFindPetsClick()
+  }
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +32,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.getThemAll}>Find pets</button>
         </div>
       </div>
     )
